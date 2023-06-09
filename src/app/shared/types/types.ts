5412fddc08035ed85af14b6ed34391a856161c76
linzequan <839860904@qq.com>
2023-06-09 18:05:03 +0800

    1

--- src/app/shared/types/types.ts
+++ src/app/shared/types/types.ts
@@ -4,12 +4,19 @@ export type ZqButtonType = 'primary' | 'default' | 'dangerous' | null
 export type ZqButtonShape = 'circle' | 'round' | null
 export type ZqButtonSize = 'large' | 'default' | 'small'
 // -----------------------------------------------
-// Select
+// 选择器类型select
 export type ZqSelectType = 'Tree' | 'Multiple' | null
 export interface ZqSelectOption {
     key?: string
     title: string
     value?: string
+    _hide?: boolean
     _checked?: boolean
     _disabled?: boolean
+}
+
+// 表格类型
+export interface ZqTableItem<T> {
+    property: keyof T
+    title: string
 }
