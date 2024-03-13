be3f9c58f4295564468077788983a0e02f035cd0
linzequan <839860904@qq.com>
2024-03-13 10:05:45 +0800

    降级v16

---
+++ src/app/docs/any-demo/demo/canvas-demo/gantetu/gantetu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gantetu',
+  standalone: true,
+  imports: [],
+  templateUrl: './gantetu.component.html',
+  styleUrl: './gantetu.component.less'
+})
+export class GantetuComponent {
+
+}
